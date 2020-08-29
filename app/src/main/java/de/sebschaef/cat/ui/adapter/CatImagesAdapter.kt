7a482e1bd61c76e8d7c5c9be1a92f0ea83f4949c
Sebastian Schäf <sebastian.schaef91@gmail.com>
2020-08-29 12:15:19 +0200

    Refactored the recyclerview adapter for better readability

--- app/src/main/java/de/sebschaef/cat/ui/adapter/CatImagesAdapter.kt
+++ app/src/main/java/de/sebschaef/cat/ui/adapter/CatImagesAdapter.kt
@@ -2,41 +2,26 @@ package de.sebschaef.cat.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import com.bumptech.glide.Glide
 import de.sebschaef.cat.R
 import de.sebschaef.cat.model.persistence.Image
 
-class CatImagesAdapter(val onFavClicked: (adapterPos: Int, image: Image, isFavoured: Boolean) -> Unit) :
-    PagingDataAdapter<Image, ImageViewHolder>(ImageDiffer) {
+class CatImagesAdapter(
+    private val onFavClicked: (adapterPos: Int, image: Image, isFavoured: Boolean) -> Unit
+) : PagingDataAdapter<Image, ImageViewHolder>(ImageDiffer) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        ImageViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_cat_image, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_cat_image,
+            parent,
+            false
         )
+        return ImageViewHolder(itemView, onFavClicked)
+    }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        val catImageView = holder.itemView.findViewById<ImageView>(R.id.iv_cat_image)
-        val favIcon = holder.itemView.findViewById<ImageView>(R.id.iv_fav)
-
-        getItem(position)?.let { image ->
-            Glide.with(catImageView)
-                .load(image.url)
-                .placeholder(R.drawable.ic_cat_half_transparent)
-                .into(catImageView)
-
-            favIcon.apply {
-                setImageResource(
-                    image.favId?.let { R.drawable.ic_star_full } ?: R.drawable.ic_star_outline
-                )
-                setOnClickListener {
-                    val setFavoured = image.favId == null
-                    onFavClicked(position, image, setFavoured)
-                }
-            }
-        }
+        getItem(position)?.let { holder.bindTo(it) } ?: holder.reset()
     }
 }
 
--- app/src/main/java/de/sebschaef/cat/ui/adapter/ImageViewHolder.kt
+++ app/src/main/java/de/sebschaef/cat/ui/adapter/ImageViewHolder.kt
@@ -1,6 +1,46 @@
 package de.sebschaef.cat.ui.adapter
 
 import android.view.View
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import de.sebschaef.cat.R
+import de.sebschaef.cat.model.persistence.Image
 
-class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class ImageViewHolder(
+    itemView: View,
+    private val onFavClicked: (adapterPos: Int, image: Image, isFavoured: Boolean) -> Unit
+) : RecyclerView.ViewHolder(itemView) {
+
+    private val catImageView: ImageView
+        get() = itemView.findViewById(R.id.iv_cat_image)
+
+    private val favIcon: ImageView
+        get() = itemView.findViewById<ImageView>(R.id.iv_fav)
+
+    fun reset() {
+        catImageView.setImageDrawable(null)
+        favIcon.apply {
+            setImageResource(R.drawable.ic_star_outline)
+            setOnClickListener(null)
+        }
+    }
+
+    fun bindTo(image: Image) {
+        Glide.with(catImageView)
+            .load(image.url)
+            .placeholder(R.drawable.ic_cat_half_transparent)
+            .into(catImageView)
+
+        favIcon.apply {
+            setImageResource(
+                image.favId?.let { R.drawable.ic_star_full } ?: R.drawable.ic_star_outline
+            )
+            setOnClickListener {
+                val setFavoured = image.favId == null
+                onFavClicked(bindingAdapterPosition, image, setFavoured)
+            }
+        }
+    }
+
+}
